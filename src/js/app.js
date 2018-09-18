b88510d4524434e10c156b4fb947ae35837f70d1
Grimmino <Grimmino@gmail.com>
2018-09-18 23:21:49 +0500

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,33 @@
-var createList = document.querySelector(".create-list");
-var popup = document.querySelector(".popup");
-var close = document.querySelector(".close");
-var addSplList = document.querySelector(".addSplList");
-var addNotSplList = document.querySelector(".addNotSplList");
-var sidebar = document.querySelector(".sidebar");
-
-createList.addEventListener("click",SelectList);
-close.addEventListener("click",closepopup);
-
-addSplList.addEventListener("click",add_SplList);
-addNotSplList.addEventListener("click",add_NotSplList);
-
+//popup
+//появление
+document.querySelector(".create-list").addEventListener("click",SelectList);
 function SelectList() {
-    popup.style = "visibility: visible";
+    document.querySelector(".popup-select-list").style = "visibility: visible";
 }
-function closepopup() {
-    popup.style = "visibility: hidden";
-}
-
-function add_SplList() {
-    var newList = document.createElement('div');
-    newList.innerHTML = '<div class="btn ">простой список</div>';
-    sidebar.appendChild(newList);
-    popup.style = "visibility: hidden";
+//закрытие
+document.querySelector(".close").addEventListener("click",ClosePopup);
+function ClosePopup() {
+    document.querySelector(".popup").style = "visibility: hidden";
 }
 
-function add_NotSplList() {
-    var newList = document.createElement('div');
-    newList.innerHTML = '<div class="btn ">не простой список</div>';
-    sidebar.appendChild(newList);
-    popup.style = "visibility: hidden";
-}
+
+
+//добавляем список
+/*var el = document.querySelectorAll("div.addList");
+console.log(el);
+
+el.addEventListener("click",addList);
+
+
+function addList() {
+    var typeList = this.className;
+    console.log(typeList);
+    var NewList = document.createElement('div');
+    NewList.className = "btn" + typeList;
+    NewList.innerHTML =
+        "<div class="+ +typeList + ">обычный список</div>" +
+        "<div class="+ +typeList + "></div>";
+
+    sidebar.appendChild(NewList);
+}*/
+
