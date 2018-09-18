1bfde88b03b48a662ed0a2847bae842029b203a0
Grimmino <Grimmino@gmail.com>
2018-09-19 00:59:10 +0500

    update js

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,27 @@
 //popup
-//появление
-document.querySelector(".create-list").addEventListener("click",SelectList);
-function SelectList() {
+document.querySelector(".create-list").addEventListener("click",ShowPopup_SelectList);
+function ShowPopup_SelectList() {
     document.querySelector(".popup-select-list").style = "visibility: visible";
 }
-//закрытие
-document.querySelector(".close").addEventListener("click",ClosePopup);
-function ClosePopup() {
+document.querySelector(".close").addEventListener("click",CloseAllPopup);
+function CloseAllPopup() {
     document.querySelector(".popup").style = "visibility: hidden";
 }
 
 
-
 //добавляем список
-/*var el = document.querySelectorAll("div.addList");
-console.log(el);
+var ChoiceList = document.querySelectorAll(".addList");
+ChoiceList.forEach( function(el) {
+    el.addEventListener("click",addList);
 
-el.addEventListener("click",addList);
+    function addList() {
+        if (el.className === "addList addSplList") {
+            alert ("создаем обычный список!")
+        } else {
+            alert ("создаем НЕобычный список!")
+        }
+    }
+});
 
 
-function addList() {
-    var typeList = this.className;
-    console.log(typeList);
-    var NewList = document.createElement('div');
-    NewList.className = "btn" + typeList;
-    NewList.innerHTML =
-        "<div class="+ +typeList + ">обычный список</div>" +
-        "<div class="+ +typeList + "></div>";
-
-    sidebar.appendChild(NewList);
-}*/
 
